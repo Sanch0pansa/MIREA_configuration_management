e4b45b3aa7f0283ccb001f3cecd3cfcf6cc74ec5
Лебедев Александр <sashaleb2006@yandex.ru>
2024-11-19 16:40:28 +0300

    Make commands config

---
+++ homework4/config.py
@@ -0,0 +1,22 @@
+CMDS = {
+    "CONST": {
+        "number": 18,
+        "layout": [5, 5, 17],
+        "size": 4,
+    },
+    "READ": {
+        "number": 27,
+        "layout": [5, 5, 28],
+        "size": 5,
+    },
+    "WRITE": {
+        "number": 13,
+        "layout": [5, 28, 5],
+        "size": 5,
+    },
+    "OR": {
+        "number": 12,
+        "layout": [5, 5, 5, 5],
+        "size": 3,
+    },
+}
